1163f92ea6e4c9dcd04bbe96e699ae2c54f60f9c
Nguyen Tung Son <titlai434321@gmail.com>
2022-12-05 03:03:58 +0700

    save

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SonKhoatnhom4.Models;
+
+namespace SonKhoatnhom4.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SonKhoatnhom4.Models.Danhsachkhachhang> Danhsachkhachhang { get; set; } = default!;
+
+        public DbSet<SonKhoatnhom4.Models.Danhsachnhanvien> Danhsachnhanvien { get; set; } = default!;
+
+        public DbSet<SonKhoatnhom4.Models.DanhsachNCC> DanhsachNCC { get; set; } = default!;
+    }
+}
